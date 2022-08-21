cb686b9df8196cfc86a360bd64cf3e74aff2b7ae
er <1364241211@qq.com>
2022-08-21 18:15:39 +0800

    修改gitignore规则

--- customersys/rest_api/customersOperation.py
+++ customersys/rest_api/customersOperation.py
@@ -24,14 +24,6 @@ class customersOp():
         tempName = zipFileName
         tempZipPath = os.path.join(self.zipFilePath, "{0}.zip".format(tempName))
         future: futureInfo = settings.GLOBAL_THREAD_POOL.future_dict.get(zipFileName)
-        # if filters:
-        #     with zipfile.ZipFile(tempZipPath, "w", zipfile.ZIP_DEFLATED) as zipFile:
-        #         for avatar in filters:
-        #             temp = str(os.path.join(self.staticPath, avatar))
-        #             try:
-        #                 zipFile.write(temp, arcname=avatar)
-        #             except FileNotFoundError:
-        #                 continue
         zipInfoList = ZipfilesInfo.objects.all()
         newZipInfo = ZipfilesInfo()
         if zipInfoList.count() == 3:
@@ -55,26 +47,70 @@ class customersOp():
         newZipInfo.last_modified_time = now()
         newZipInfo.download = 0
         newZipInfo.save()
-        a = os.listdir(self.staticPath)
-        with zipfile.ZipFile(tempZipPath, "w", zipfile.ZIP_DEFLATED) as zipFile:
-            for at in a:
-                temp = str(os.path.join(self.staticPath, at))
-                fileSize = os.path.getsize(tempZipPath)
-                newZipInfo.zip_size = fileSize
-                try:
-                    zipFile.write(temp, arcname=at)
-                    newZipInfo.save()
-                    if future.single == singleEnum.SHUTDOWN:
-                        raise CancelledError("线程已终止")
-                except FileNotFoundError:
-                    continue
-                except CancelledError:
-                    with self.sync_lock:
-                        if os.path.exists(tempZipPath):
-                            os.remove(tempZipPath)
-                            newZipInfo.download = 0
-                            newZipInfo.save()
-                    break
+        if filters:
+            with zipfile.ZipFile(tempZipPath, "w", zipfile.ZIP_DEFLATED) as zipFile:
+                for avatar in filters:
+                    temp = str(os.path.join(self.staticPath, avatar))
+                    fileSize = os.path.getsize(tempZipPath)
+                    newZipInfo.zip_size = fileSize
+                    try:
+                        zipFile.write(temp, arcname=avatar)
+                        newZipInfo.save()
+                        if future.single == singleEnum.SHUTDOWN:
+                            raise CancelledError("线程已终止")
+                    except FileNotFoundError:
+                        continue
+                    except CancelledError:
+                        with self.sync_lock:
+                            if os.path.exists(tempZipPath):
+                                os.remove(tempZipPath)
+                                newZipInfo.download = 0
+                                newZipInfo.save()
+                        break
+
+        # zipInfoList = ZipfilesInfo.objects.all()
+        # newZipInfo = ZipfilesInfo()
+        # if zipInfoList.count() == 3:
+        #     minZipInfo = ZipfilesInfo()
+        #     minTime = min([t.last_modified_time for t in zipInfoList])
+        #     for i, v in enumerate(zipInfoList):
+        #         if v.last_modified_time == minTime:
+        #             minZipInfo: ZipfilesInfo = zipInfoList[i]
+        #             break
+        #     newZipInfo = minZipInfo
+        #     if newZipInfo.download == 0:
+        #         tempFuture: futureInfo = settings.GLOBAL_THREAD_POOL.future_dict.get(
+        #             os.path.splitext(newZipInfo.zip_name)[0])
+        #         tempFuture.single = singleEnum.SHUTDOWN
+        #     with self.sync_lock:
+        #         tempRemoveFilePath = os.path.join(self.zipFilePath, newZipInfo.zip_name)
+        #         if os.path.exists(tempRemoveFilePath):
+        #             os.remove(tempRemoveFilePath)
+        # newZipInfo.zip_name = "{0}.zip".format(tempName)
+        # newZipInfo.zip_size = 0
+        # newZipInfo.last_modified_time = now()
+        # newZipInfo.download = 0
+        # newZipInfo.save()
+        # a = os.listdir(self.staticPath)
+        # with zipfile.ZipFile(tempZipPath, "w", zipfile.ZIP_DEFLATED) as zipFile:
+        #     for at in a:
+        #         temp = str(os.path.join(self.staticPath, at))
+        #         fileSize = os.path.getsize(tempZipPath)
+        #         newZipInfo.zip_size = fileSize
+        #         try:
+        #             zipFile.write(temp, arcname=at)
+        #             newZipInfo.save()
+        #             if future.single == singleEnum.SHUTDOWN:
+        #                 raise CancelledError("线程已终止")
+        #         except FileNotFoundError:
+        #             continue
+        #         except CancelledError:
+        #             with self.sync_lock:
+        #                 if os.path.exists(tempZipPath):
+        #                     os.remove(tempZipPath)
+        #                     newZipInfo.download = 0
+        #                     newZipInfo.save()
+        #             break
         newZipInfo.download = 1
         newZipInfo.save()
         return tempZipPath, fileSize
